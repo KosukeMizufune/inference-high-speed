f55bd34aa214017728b4635ca158004fed70d790
kosuke <mfs.coldvdgas4645@gmail.com>
2019-09-20 01:06:24 +0900

    first commit

--- src/infer_with_npz.py
+++ src/infer_with_npz.py
@@ -1,13 +1,16 @@
+import argparse
+
 import chainer
-from chainer import serializers, iterators
+from chainer import serializers
 from chainercv.links import SSD300
-from chainercv.evaluations import eval_detection_voc
-from chainercv.extensions import DetectionVOCEvaluator
-from chainercv.datasets import voc_bbox_label_names
-from chainercv.datasets.voc.voc_bbox_dataset import VOCBboxDataset
+from PIL import Image
+import numpy as np
+
+from utils import time_watch
 
 
-def infer(model_path, data_dir=None, gpu_id=0):
+@time_watch
+def infer(model_path, img_path=None, gpu_id=0):
     model = SSD300(n_fg_class=20)
     model.use_preset('evaluate')
     if model_path.endswith('.npz'):
@@ -21,11 +24,18 @@ def infer(model_path, data_dir=None, gpu_id=0):
         chainer.cuda.get_device_from_id(0).use()
         model.to_gpu(gpu_id)
 
-    test = VOCBboxDataset(year='2007', split='test', use_difficult=True, return_difficult=True)
-    test_iter = iterators.SerialIterator(test, 32, repeat=False, shuffle=False)
-    evaluator = DetectionVOCEvaluator(test_iter, model, use_07_metric=True, label_names=voc_bbox_label_names)
-    print(evaluator.evaluate())
+    img = Image.open(img_path)
+    img = np.array(img, dtype=np.float32).transpose(2, 0, 1)
+    img = img[np.newaxis, ...]
+
+    _ = model(img)
 
 
 if __name__ == "__main__":
-    infer('../models/snapshot_ssd', gpu_id=-1)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_path', type=str, default=None)
+    parser.add_argument('--img_path', type=str, default=None)
+    parser.add_argument('--gpu_id', type=int, default=0)
+
+    args = parser.parse_args()
+    infer(args.model_path, args.img_path, args.gpu_id)
