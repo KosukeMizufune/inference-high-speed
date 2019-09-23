937d39dc2897b61f51e3b290ddfe45c122e53c7a
kosuke <mfs.coldvdgas4645@gmail.com>
2019-09-23 20:57:03 +0900

    first commit

--- src/classification/resnet/infer_resnet_with_npz.py
+++ src/classification/resnet/infer_resnet_with_npz.py
@@ -1,14 +1,11 @@
 import argparse
 
-import chainer
-from chainer import serializers, cuda
+from chainer import cuda, Chain, initializers, serializers
 from chainer import links as L
-from chainercv.links import SSD300
 from PIL import Image
 import numpy as np
 
-from utils import stop_watch
-
+from local_lib.utils import stop_watch
 
 @stop_watch
 def infer(img, model):
@@ -17,13 +14,13 @@ def infer(img, model):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_path', type=str, default=None)
+    
     parser.add_argument('--img_path', type=str, default=None)
     parser.add_argument('--gpu_id', type=int, default=0)
 
     args = parser.parse_args()
 
-    model = L.ResNet101Layers(pretrained_model='auto')
+    model = L.VGG16Layers()
 
     if args.gpu_id >= 0:
         cuda.get_device_from_id(args.gpu_id).use()
@@ -31,6 +28,7 @@ if __name__ == "__main__":
 
     img = Image.open(args.img_path)
     img = np.array(img, dtype=np.float32).transpose(2, 0, 1)
+    img = L.model.vision.vgg.prepare(img)
     img = img[np.newaxis, ...]
 
     infer(img, model)
