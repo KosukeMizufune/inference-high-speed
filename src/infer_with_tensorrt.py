939720aa5e3127d904361dc666299a2f932bba53
kosuke <mfs.coldvdgas4645@gmail.com>
2019-09-24 22:54:03 +0900

    fix bug

--- src/infer_with_tensorrt.py
+++ src/infer_with_tensorrt.py
@@ -26,12 +26,12 @@ def do_inference(context, bindings, inputs, outputs, stream, batch_size=1):
         batch_size=batch_size, bindings=bindings
     )
     [cuda.memcpy_dtoh(out.host, out.device) for out in outputs]
-    return [out.host for out in outputs], infer_time
+    return [out.host for out in outputs]
 
 
-def transform_img(img, insize=300):
+def transform_img(img, img_size):
     img = np.array(img, dtype=np.float32).transpose(2, 0, 1)
-    img = transforms.resize(img, (insize, insize))
+    img = transforms.resize(img, (img_size, img_size))
     img -= np.array((123, 117, 104)).reshape((-1, 1, 1))
     return img
 
@@ -40,16 +40,17 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--engine_path', type=str, default=None)
     parser.add_argument('--img_path', type=str, default=None)
+    parser.add_argument('--img_size', int, default=224)
 
     args = parser.parse_args()
 
     img = Image.open(args.img_path)
-    img = transform_img(img)
+    img = transform_img(img, args.img_size)
 
     with load_engine(args.engine_path) as engine:
         with engine.create_execution_context() as context:
             inputs, outputs, bindings, stream = common.allocate_buffers(engine)
             inputs[0].host = img
-            trt_outputs, infer_time = do_inference(
+            trt_outputs = do_inference(
                 context, bindings=bindings, inputs=inputs, outputs=outputs, stream=stream
             )
