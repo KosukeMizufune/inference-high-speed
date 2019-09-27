d4074c9e59e218d489316ab711b0dff913d84206
kosuke <mfs.coldvdgas4645@gmail.com>
2019-09-27 23:34:43 +0900

    gitkeepを無視しないようにする

---
+++ build/lib/local_lib/utils.py
@@ -0,0 +1,26 @@
+from functools import wraps
+import logging
+from logging import getLogger, FileHandler, Formatter
+import time
+import os
+
+
+def stop_watch(func):
+    logger = getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    if not os.path.exists('log'):
+        os.mkdir('log')
+    file_handler = FileHandler('log/inference.log')
+    handler_format = Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(handler_format)
+    logger.addHandler(file_handler)
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        elapsed_itme = time.time() - start_time
+        logger.info(elapsed_itme)
+        return result
+
+    return wrapper
